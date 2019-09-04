100793e81e3de631ec980c659b674e1d5056b405
yamatatsu <the.kinnikuman@gmail.com>
2019-09-04 10:24:39 +0900

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -11,45 +11,45 @@ import remarkHtml from "remark-html"
 
 import { Post } from "./type"
 
-const filenames = fs.readdirSync(`${__dirname}/../posts`)
+const postPath = `${__dirname}/../posts`
+const distPath = `${__dirname}/../public`
 
-const posts = filenames.map<Post>(filename => {
-  const md = fs.readFileSync(`${__dirname}/../posts/${filename}`, "utf-8")
+const posts: Post[] = fs.readdirSync(postPath).map(filename => {
+  const md = fs.readFileSync(`${postPath}/${filename}`, "utf-8")
+  const postHeader = getPostHeader(md)
+  const html = getHtml(md)
+  return { filename: filename.replace(/\.md$/, ""), postHeader, contents: html }
+})
 
+loadTemplate("index.ejs", { posts })
+  .then(str => fs.promises.writeFile(`${distPath}/index.html`, str))
+  .catch(err => console.error(err))
+
+posts.forEach(post => {
+  const dirPath = `${distPath}/${post.filename}`
+  fs.promises
+    .mkdir(dirPath)
+    .then(() => loadTemplate("post.ejs", { post }))
+    .then(str => fs.promises.writeFile(`${dirPath}/index.html`, str))
+    .catch(err => console.error(err))
+})
+
+function getPostHeader(md: string): Record<string, string> {
   const yamlNode = (unified()
     .use(parse)
     .use(frontmatter)
     .parse(md) as Parent).children.find(node => node.type === "yaml")
-  const postHeader = yamlNode && yaml.safeLoad(yamlNode.value as string)
+  return yamlNode && yaml.safeLoad(yamlNode.value as string)
+}
 
+function getHtml(md: string): string {
   const contents = unified()
     .use(parse)
     .use(frontmatter)
     .use(remarkHtml)
     .processSync(md).contents as string
-
-  return { filename: filename.replace(/\.md$/, ""), postHeader, contents }
-})
-
-loadTemplate("index.ejs", { posts })
-  .then(str => {
-    fs.writeFileSync(`${__dirname}/../public/index.html`, str)
-  })
-  .catch(err => console.error(err))
-
-posts.forEach(post => {
-  loadTemplate("post.ejs", { post })
-    .then(str => {
-      fs.mkdirSync(`${__dirname}/../public/${post.filename}`, {
-        recursive: true,
-      })
-      fs.writeFileSync(
-        `${__dirname}/../public/${post.filename}/index.html`,
-        str
-      )
-    })
-    .catch(err => console.error(err))
-})
+  return contents
+}
 
 function loadTemplate(path: string, data: Record<string, any>) {
   return ejs.renderFile(`${__dirname}/../ejs/${path}`, data, {
